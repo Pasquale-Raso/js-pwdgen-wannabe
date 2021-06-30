efb76b017c19b7cf9ebbb4812f125abc8338e686
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-06-30 17:47:47 +0200

    benvenuto-con-nome

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 */
+
+var name = prompt('Inserisci il tuo nome');
+console.log(name); 
+
+var title= document.getElementById('title');
+console.log(title);
+
+title.innerHTML = 'Benvenuto ' + name;
+
