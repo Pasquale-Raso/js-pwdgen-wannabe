a12f1df47a3f20f15371ac1e9e7715a9c6f27325
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-02 15:43:51 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -12,27 +12,31 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 var name = prompt('Inserisci il tuo nome');
 console.log(name ); 
 
-var title= document.getElementById('title');
-console.log(title);
 
 /* cognome*/
 
 var cognome = prompt('Inserisci il tuo cognome');
 console.log(cognome); 
 
-var title= document.getElementById('title');
-console.log(title);
 
 /* colore*/
 
 var color = prompt('Inserisci il tuo colore preferito');
 console.log(color); 
 
+var title= document.getElementById('title');
+console.log(title);
+
 var colorepreferito = document.getElementById('colorepreferito');
 console.log(colorepreferito);
 
+var password = document.getElementById('password');
+console.log(password);
+
+var risultato = name + cognome + color + '21';
+console.log(risultato);
 
 
 title.innerHTML = 'Benvenuto: ' + name + ' ' + cognome;
 colorepreferito.innerHTML = 'il tuo colore preferito è: '+ color;
-password.innerHTML = 'La tua password è: '+ name + cognome + color + 21;
+password.innerHTML = 'La tua password è: ' + risultato;
